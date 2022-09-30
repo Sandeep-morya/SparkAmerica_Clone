61fbec2735f14cf128cba79692015bf618f8a3cf
Sandeep-morya <laxmidassmorya>
2022-09-30 15:11:31 +0530

    beta upload done

--- Scripts/logo.js
+++ Scripts/logo.js
@@ -1,3 +1,9 @@
 document.querySelector("#logo>img").addEventListener("click", (e) => {
     window.location.href ="./Index.html";
 });  
+document.querySelector("#fb").addEventListener("click",e=>{
+    window.location.reload();
+});
+document.querySelector("#insta").addEventListener("click",e=>{
+    window.location.reload();
+});
